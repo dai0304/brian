19882f321999040c130bc728c5def3468b87e406
Daisuke Miyamoto <miyamoto.daisuke@classmethod.jp>
2014-04-10 16:57:24 +0900

    remove unuesed import statements

--- src/main/java/jp/classmethod/aws/brian/web/HomeController.java
+++ src/main/java/jp/classmethod/aws/brian/web/HomeController.java
@@ -15,8 +15,6 @@
  */
 package jp.classmethod.aws.brian.web;
 
-import org.quartz.Scheduler;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
